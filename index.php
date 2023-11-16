dc3c1b6ee21626ec6bcc3c21cfb5d28cce2ad583
Cameron Cardinal <cameriancardinal@gmail.com>
2023-11-16 11:12:31 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html>
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link href="/dist/output.css" rel="stylesheet">
+</head>
+<body>
+  <h1 class="text-3xl font-bold underline text-yellow-600">
+    Hello world!
+  </h1>
+  <p class="text-red-600">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam corporis aut beatae nam expedita omnis, architecto officiis commodi unde in. Voluptates vel assumenda exercitationem et sit iusto aperiam cum sequi.</p>
+</body>
+</html>
